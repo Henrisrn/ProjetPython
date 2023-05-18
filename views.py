e69bf753c2667e878a4734badffe7c3d3ac66845
Henri Serano <107102389+Henrisrn@users.noreply.github.com>
2023-05-18 23:28:32 +0200

    Update views.py

--- views.py
+++ views.py
@@ -40,6 +40,7 @@ print(data)
 def index(request):
     #TEST DE GRAPH SYMPA
     # Embed plots into HTML via Flask Render
+    
     scatter_plot = px.scatter(data, x="Surface terrain", y="PrixInt",
                           title="Nuage de points", labels={"x": "Valeur X", "y": "Valeur Y"})
 
@@ -159,6 +160,133 @@ def index(request):
         title="Répartition des types locaux des biens"
     )
 
+    # %%
+    # Convertir 'Date mutation' en datetime
+    data['Date mutation'] = pd.to_datetime(data['Date mutation'], format='%d/%m/%Y')
+    data['Date mutation'] = pd.to_datetime(data['Date mutation'], format='%d/%m/%Y')
+
+    # Ajouter la colonne 'Mois'
+    data['Mois'] = data['Date mutation'].dt.month
+    data['Mois'] = data['Date mutation'].dt.month
+
+    # Calculer l'IQR pour 'Valeur fonciere' dans les deux jeux de données
+    Q1_1 = data['PrixFloat'].quantile(0.25)
+    Q3_1 = data['PrixFloat'].quantile(0.75)
+    IQR_1 = Q3_1 - Q1_1
+
+    Q1_2 = data['PrixFloat'].quantile(0.25)
+    Q3_2 = data['PrixFloat'].quantile(0.75)
+    IQR_2 = Q3_2 - Q1_2
+
+    # Définir les limites pour les valeurs aberrantes dans les deux jeux de données
+    limite_inferieure_1 = Q1_1 - 1.5 * IQR_1
+    limite_superieure_1 = Q3_1 + 1.5 * IQR_1
+
+    limite_inferieure_2 = Q1_2 - 1.5 * IQR_2
+    limite_superieure_2 = Q3_2 + 1.5 * IQR_2
+
+    # Filtrer les données
+    donnees_filtrees_1 = data[(data['PrixFloat'] >= limite_inferieure_1) & (data['PrixFloat'] <= limite_superieure_1)]
+    donnees_filtrees_2 = data[(data['PrixFloat'] >= limite_inferieure_2) & (data['PrixFloat'] <= limite_superieure_2)]
+
+
+
+    # Convertir 'Date mutation' en datetime
+    # Votre code ici...
+
+    # Boîte à moustaches pour la première année
+    fig7 = px.box(donnees_filtrees_1, x='Mois', y='PrixFloat', title='Boîte à moustaches de "Valeur fonciere" par mois (valeurs aberrantes supprimées) - Année 1')
+
+    # Boîte à moustaches pour la deuxième année
+    fig8 = px.box(donnees_filtrees_2, x='Mois', y='PrixFloat', title='Boîte à moustaches de "Valeur fonciere" par mois (valeurs aberrantes supprimées) - Année 2')
+    donnees_filtrees = pd.concat([donnees_filtrees_1, donnees_filtrees_2])
+
+    # Créer une nouvelle colonne 'Année' dans les données filtrées
+    donnees_filtrees['Annee'] = donnees_filtrees['Date mutation'].dt.year
+    # Boîte à moustaches pour les deux années superposées
+    fig9 = px.box(donnees_filtrees, x='Mois', y='PrixFloat', color='Annee', title='Boîte à moustaches de "Valeur fonciere" par mois (valeurs aberrantes supprimées), superposition des années')
+    # Diagramme en barres du Nombre de Pièces Principales
+    fig10 = px.histogram(data, x='Nombre pieces principales', nbins=int(max(data['Nombre pieces principales'].fillna(0))), title='Nombre de propriétés par nombre de pièces principales en 2022', color_discrete_sequence=['blue'])
+
+
+    
+    data['Nombre pieces principales'].value_counts().sort_index().plot(kind='bar', color='blue', alpha=0.5, label="2022")
+
+   
+
+    
+    data['Type local'].value_counts().plot(kind='pie', autopct='%1.1f%%')
+    data['Date mutation'] = pd.to_datetime(data['Date mutation'], format='%d/%m/%Y')
+    # Diagramme en Camembert du Type Local
+    fig12 = px.pie(data, names='Type local', title='Répartition des types de locaux en 2022')
+
+
+    # Calcule l'IQR pour 'Surface reelle bati'
+    Q1 = data['Surface reelle bati'].quantile(0.25)
+    Q3 = data['Surface reelle bati'].quantile(0.75)
+    IQR = Q3 - Q1
+
+    # Définis les limites pour les valeurs aberrantes
+    limite_inférieure = Q1 - 1.5 * IQR
+    limite_supérieure = Q3 + 1.5 * IQR
+
+    # Filtre les données
+    data_filtrée = data[(data['Surface reelle bati'] >= limite_inférieure) & (data['Surface reelle bati'] <= limite_supérieure)]
+
+    # Extrait le mois de la colonne 'Date mutation'
+    data_filtrée['Mois'] = data_filtrée['Date mutation'].dt.month
+    
+    fig13 = px.pie(data, names='Type local', title='Répartition des types de locaux en 2019')
+
+    data['Date mutation'] = pd.to_datetime(data['Date mutation'], format='%d/%m/%Y')
+    # Diagramme en boîte pour "Surface reelle bati" par mois
+    fig14 = px.box(data_filtrée, x='Mois', y='Surface reelle bati', title='Diagramme en boîte de "Surface Reelle Bati" par Mois (valeurs aberrantes enlevées)')
+
+
+    # Calcule l'IQR pour 'Surface reelle bati'
+    Q1 = data['Surface reelle bati'].quantile(0.25)
+    Q3 = data['Surface reelle bati'].quantile(0.75)
+    IQR = Q3 - Q1
+
+    # Définis les limites pour les valeurs aberrantes
+    limite_inférieure = Q1 - 1.5 * IQR
+    limite_supérieure = Q3 + 1.5 * IQR
+
+    # Filtre les données
+    data_filtrée = data[(data['Surface reelle bati'] >= limite_inférieure) & (data['Surface reelle bati'] <= limite_supérieure)]
+
+    # Extrait le mois de la colonne 'Date mutation'
+    data_filtrée['Mois'] = data_filtrée['Date mutation'].dt.month
+
+    
+    fig15 = px.box(donnees_filtrees_1, x='Mois', y='Valeur fonciere', 
+               title='Boîte à moustaches de "Valeur fonciere" par mois (valeurs aberrantes supprimées) - Année 1')
+
+
+    # Boîte à moustaches pour la deuxième année
+    fig16 = px.box(donnees_filtrees_2, x='Mois', y='Valeur fonciere',
+                title='Boîte à moustaches de "Valeur fonciere" par mois (valeurs aberrantes supprimées) - Année 2')
+
+
+    # Boîte à moustaches superposition des années
+    fig17 = px.box(donnees_filtrees, x='Mois', y='Valeur fonciere', color='Annee',
+                title='Boîte à moustaches de "Valeur fonciere" par mois (valeurs aberrantes supprimées), superposition des années')
+
+
+    # Pie chart du Type Local
+    fig19 = px.pie(data, names='Type local',
+                title='Répartition des types de locaux en 2022')
+
+
+    fig20 = px.pie(data, names='Type local',
+                title='Répartition des types de locaux en 2019')
+
+
+    # Boîte à moustaches de "Surface Reelle Bati" par Mois (valeurs aberrantes enlevées)
+    fig21 = px.box(data_filtrée, x='Mois', y='Surface reelle bati',
+                title='Diagramme en boîte de "Surface Reelle Bati" par Mois (valeurs aberrantes enlevées)')
+
+    
     app = DjangoDash('app', add_bootstrap_links=True)
     app.layout = html.Div([
     html.Div([
@@ -169,6 +297,20 @@ def index(request):
         dcc.Graph(id='graph5', figure=fig4),
         dcc.Graph(id='graph6', figure=fig5),
         dcc.Graph(id='graph7', figure=fig6),
+        dcc.Graph(id='graph3', figure=fig7),
+        dcc.Graph(id='graph4', figure=fig8),
+        dcc.Graph(id='graph5', figure=fig9),
+        dcc.Graph(id='graph6', figure=fig10),
+        dcc.Graph(id='graph8', figure=fig12),
+        dcc.Graph(id='graph8', figure=fig13),
+        dcc.Graph(id='graph10', figure=fig14),
+        dcc.Graph(id='graph11', figure=fig15),
+        dcc.Graph(id='graph12', figure=fig16),
+        dcc.Graph(id='graph13', figure=fig17),
+        dcc.Graph(id='graph15', figure=fig19),
+        dcc.Graph(id='graph16', figure=fig20),
+        dcc.Graph(id='graph17', figure=fig21),
+        
     ]),
 ])
 
